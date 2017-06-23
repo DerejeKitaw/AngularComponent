af2d5d557b3eca1f5da51aa9965d9c40b0f2b72b
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-22 23:59:36 -0400

    added: questions list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DUMMY_DATA } from "../../api/data/dummy-data";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  questions = DUMMY_DATA;
+  
 }
