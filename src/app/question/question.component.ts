6ea84f57fece13a938a2cc77fb2ac9f7ad45bc15
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-23 00:45:40 -0400

    updated: question component

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Question } from "app/models/model";
 
 @Component({
   selector: 'question',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
-
+@Input() question: Question;
   constructor() { }
 
   ngOnInit() {
