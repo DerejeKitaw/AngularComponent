dac839dde942a7f122f4459d0ab36baac292a8c3
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-24 13:22:40 -0400

    update: question component recive question data from hosting component

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Question } from "app/models/model";
 
 @Component({
   selector: 'app-question',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
+  // question component will get questions from hosting component
+  @Input() question: Question;
 
   constructor() { }
 
