c74633de5481bc0c36f3948a69b4965728d28392
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-24 12:07:38 -0400

    component added: question , choice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
+import { ChoiceComponent } from './components/choice/choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    ChoiceComponent
   ],
   imports: [
     BrowserModule
