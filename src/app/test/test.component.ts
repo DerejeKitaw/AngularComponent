9366862c3616a688f127321958be99dd1c90691c
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-24 23:25:47 -0400

    added: test component

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { DUMMY_DATA } from "app/data/dummy-data";
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent {
+ questions = DUMMY_DATA;
+  summary = {};
+  selections = {};
+  showSummary = false;
+  option = false;
+  activeSlideIndex = 0;
+
+  constructor() {
+  }
+
+  onSelected(questionIdx, choiceIdx) {
+    this.summary[questionIdx] =
+      this.questions[questionIdx].choices[choiceIdx].summary;
+
+    this.selections[questionIdx] = choiceIdx;
+
+    if (Object.keys(this.selections).length === this.questions.length) {
+      this.activeSlideIndex = 0;
+      this.showSummary = true;
+    } else {
+      // FOR AUTO change question
+      if (this.activeSlideIndex === this.questions.length - 1 && this.option) {
+        this.activeSlideIndex = 0;
+      } else if (this.option) {
+        this.activeSlideIndex += 1;
+      }
+    }
+  }
+
+  changeActiveSlide(index) {
+    this.activeSlideIndex = index;
+  }
+
+  resetQuiz() {
+    this.showSummary = false;
+    this.selections = {};
+    this.summary = {};
+  }
+
+  getSummary() {
+    const limit = Object.keys(this.summary).length;
+    let summary = '';
+
+    for (let index = 0; index < limit; index++) {
+      summary += `${this.summary[index]}`;
+    }
+
+    return summary;
+  }
+}
